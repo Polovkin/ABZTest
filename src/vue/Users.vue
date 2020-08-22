788345de1de24273d5cca7667a2c0f8ec85fb13e
Polovkin <zimboroda@gmail.com>
2020-08-22 20:29:03 +0300

    users done

--- src/vue/Users.vue
+++ src/vue/Users.vue
@@ -1,19 +1,47 @@
 <template lang="pug">
-
   #users
-    h1 Users
+    .users__content
+      template(v-for="user of users")
+        UserItem(:user-data="user")
+    button.button-primary(
+      v-if="buttonState",
+      @click="showMore",
+      :class="{'button-primary--disabled':buttonDisable}") Show more
 
 
 </template>
 
 <script>
+import UserItem from './components/app/User-item.vue'
+
 export default {
   name: 'Users',
+  components: {UserItem},
   data() {
-    return {}
+    return {
 
+    }
   },
-  computed: {}
+  mounted() {
+    this.$store.dispatch('GET_USERS_DATA');
+  },
+  computed: {
+    users() {
+      return this.$store.getters.GET_USERS;
+    },
+    buttonDisable() {
+      return this.$store.getters.GET_BUTTON_STATUS;
+    },
+    buttonState() {
+      return this.$store.getters.GET_BUTTON_STATE;
+    },
+  },
+  methods: {
+     showMore() {
+
+      this.$store.dispatch('GET_MORE_USERS')
+    }
+  },
 };
 
 </script>
---
+++ src/vue/components/app/User-item.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  .user__card
+    header.user__header
+      img.user__photo(:src="UserData.photo")
+      h4.user__name {{UserData.name}}
+    section.user__body
+      p.user__position {{UserData.position}}
+      .user__email(:data-email="UserData.email")
+        p.user__email-text {{UserData.email}}
+      p.user__phone {{UserData.phone}}
+
+
+</template>
+
+<script>
+export default {
+name: "User-item.vue",
+  props: {
+    UserData: {},
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
