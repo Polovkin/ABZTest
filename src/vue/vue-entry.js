92075faea5415697bed9565f89354b1030ce5fff
Polovkin <zimboroda@gmail.com>
2020-08-22 10:13:59 +0300

    init

---
+++ src/vue/vue-entry.js
@@ -0,0 +1,13 @@
+//import '@babel/polyfill';
+import Vue from 'vue';
+import App from './App.vue';
+import store from './vuex/vues-store';
+
+Vue.config.productionTip = false;
+
+new Vue({
+    store,
+    render: (h) => h(App),
+}).$mount('#app');
+
+
---
+++ webpack/index.js
@@ -0,0 +1,14 @@
+// JS
+import '../src/js/utils/lazyloading';
+import '../src/js/scripts/custom';
+import '../src/js/project/modules/forms'
+
+// SCSS
+import '@/assets/scss/main.scss';
+
+// Vue.js
+import '@/vue/vue-entry';
+// CSS
+
+//Vendors
+import 'normalize.css';
